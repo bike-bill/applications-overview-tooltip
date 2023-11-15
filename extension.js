6c66c37815b4a97418ad15ea8ec495c4656f933c
Raphaël Rochet <raphael@rri.fr>
2023-11-15 21:47:13 +0100

    Big rewrite for Gnome 45

--- extension.js
+++ extension.js
@@ -1,14 +1,13 @@
-const Main = imports.ui.main;
-const Mainloop = imports.mainloop;
-const St = imports.gi.St;
-const Clutter = imports.gi.Clutter;
-const Gio = imports.gi.Gio
-const Pango = imports.gi.Pango;
-const extension = imports.misc.extensionUtils.getCurrentExtension();
-const ExtensionUtils = imports.misc.extensionUtils;
-const Format = imports.format;
-const Gettext = imports.gettext.domain('applications-overview-tooltip');
-const _ = Gettext.gettext;
+import Gio from 'gi://Gio';
+import Clutter from 'gi://Clutter';
+import St from 'gi://St';
+import Pango from 'gi://Pango';
+import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Search from 'resource:///org/gnome/shell/ui/search.js';
+import * as IconGrid from 'resource:///org/gnome/shell/ui/iconGrid.js';
+import * as Util from 'resource:///org/gnome/shell/misc/util.js';
+import {Extension, gettext as _, ngettext as __} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 // options
 let LABELSHOWTIME	= 15/100;
@@ -22,324 +21,264 @@ let GROUPAPPCOUNT	= true;
 let BORDERS			= false;
 let KEYBOARD		= true;
 
-// private variables
-let _old_addItem = null;		// used to restore monkey patched function on disable
-let _old_searchAddItem = null;	// same but for search results
-let _tooltips = null;			// used to disconnect events on disable
-let _labelTimeoutId = 0;		// id of timer waiting for start
-let _resetHoverTimeoutId = 0;	// id of last (cancellable) timer
-let _ttbox = null;				// actor for displaying the tooltip
-let _ttlayout = null;
-let _ttlabel = null;			// tooltip label
-let _ttdetail = null;			// tooltip description label
-let _labelShowing = false;		// self explainatory
+export default class ApplicationOverviewTooltipExtension extends Extension {
 
-let _settings;
-let _settingsConnectionId;
-let _ovhidingConnectionId;
-
-function init() {
-
-	// Translation init
-	String.prototype.format = Format.format;
-	ExtensionUtils.initTranslations("applications-overview-tooltip");
-
-}
-
-
-function enable() {
-
-	// Settings access
-	_settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.applications-overview-tooltip');
-	_applySettings();
-	_tooltips = new Array();
-
-	// Enabling tooltips for already loaded icons
-	_connectAll(Main.overview._overview._controls._appDisplay);
-
-	// monkeypatching for future app icons
-	_old_addItem = imports.ui.iconGrid.IconGrid.prototype.addItem;
-	imports.ui.iconGrid.IconGrid.prototype.addItem = function(item, page, index){
-		_connect(item);
-		// original part of the function I'm overwriting
-		_old_addItem.apply(this, arguments);
-	};
-
-	// monkeypatching for future app icons in search results
-	_old_searchAddItem = imports.ui.search.GridSearchResults.prototype._addItem;
-	imports.ui.search.GridSearchResults.prototype._addItem = function(display){
-		_connect(display);
-		_old_searchAddItem.apply(this, arguments);
-	};
-
-	// apply new settings if changed
-	_settingsConnectionId = _settings.connect('changed', _applySettings);
-
-	// Hide tooltip if overview is hidden
-	_ovhidingConnectionId = Main.overview.connect('hiding', _onLeave);
-
-}
-
-
-function disable() {
-
-	// Disconnect from events
-	if (_ovhidingConnectionId > 0) _settings.disconnect(_ovhidingConnectionId);
-
-	// disconnects settings
-	if (_settingsConnectionId > 0) _settings.disconnect(_settingsConnectionId);
-	_settings = null;
-
-	// restore the original addItem functions and remove references to them
-	imports.ui.iconGrid.IconGrid.prototype.addItem = _old_addItem;
-	imports.ui.search.GridSearchResults.prototype._addItem = _old_searchAddItem;
-	_old_addItem = null;
-	_old_searchAddItem = null;
-
-	// disconnects from all loaded icons
-	for (let i = 0; i < _tooltips.length; i++) {
-		_tooltips[i].actor.disconnect(_tooltips[i].con_d);
-		_tooltips[i].actor.disconnect(_tooltips[i].con_h);
-		_tooltips[i].actor.disconnect(_tooltips[i].con_focus_in);
-		_tooltips[i].actor.disconnect(_tooltips[i].con_focus_out);
+	constructor(metadata) {
+		super(metadata);
 	}
-	_tooltips=null;
 
-}
+	init() {
+		String.prototype.format = Format.format;
+	}
 
+	enable() {
+		// Some default values for private variables
+		this._old_addItem = null;		// used to restore monkey patched function on disable
+		this._old_searchAddItem = null;	// same but for search results
+		this._labelTimeoutId = 0;		// id of timer waiting for start
+		this._resetHoverTimeoutId = 0;	// id of last (cancellable) timer
+		this._ttbox = null;				// actor for displaying the tooltip
+		this._ttlayout = null;
+		this._ttlabel = null;			// tooltip label
+		this._ttdetail = null;			// tooltip description label
+		this._labelShowing = false;		// self explainatory
+		// Load settings
+		this._settings = this.getSettings();
+		this._settingsConnectionId = this._settings.connect('changed', this._applySettings.bind(this));
+		this._applySettings();
+		// Memorize all connections to events to remove them later if needed
+		this._tooltips = new Array();
+		// Enabling tooltips for already loaded icons
+		this._connectAll(Main.overview._overview._controls._appDisplay);
+		// Hide tooltip when overview is hidden
+		this._ovHidingConnectionId = Main.overview.connect('hiding', this._onLeave.bind(this));
+		// monkypatching icon views to be able to link on future icons
+		let that = this;
+		// ... in app icons view
+		this._old_addItem = IconGrid.IconGrid.prototype.addItem;
+		IconGrid.IconGrid.prototype.addItem = function(item, page, index){
+			that._connect(item); // connect to new icon
+			that._old_addItem.apply(this, arguments); // then call original function
+		};
+		// ... in search results
+		this._old_searchAddItem = Search.GridSearchResults.prototype._addItem;
+		Search.GridSearchResults.prototype._addItem = function(display){
+			that._connect(display);
+			that._old_searchAddItem.apply(this, arguments);
+		};
+	}
 
-function _applySettings() {
-
-	LABELSHOWTIME = _settings.get_int("labelshowtime")/100 ;
-	LABELHIDETIME = _settings.get_int("labelhidetime")/100 ;
-	HOVERDELAY = _settings.get_int("hoverdelay") ;
-	TITLE = _settings.get_boolean("title") ;
-	APPDESCRIPTION = _settings.get_boolean("appdescription") ;
-	GROUPAPPCOUNT = _settings.get_boolean("groupappcount") ;
-	BORDERS = _settings.get_boolean("borders");
-	KEYBOARD = _settings.get_boolean("keyboard");
-
-}
-
-
-function _connectAll(view) {
-
-	let appIcons = view._orderedItems;
-	for (let i in appIcons) {
-		let icon = appIcons[i];
-		let actor = icon;
-		if (actor._delegate.hasOwnProperty('_folder')) {
-			_connectAll(icon.view)
+	disable() {
+		// restore the original addItem functions and remove our references to them
+		IconGrid.IconGrid.prototype.addItem = this._old_addItem;
+		Search.GridSearchResults.prototype._addItem = this._old_searchAddItem;
+		this._old_addItem = null;
+		this._old_searchAddItem = null;
+		// Disconnect from all events
+		if (this._settingsConnectionId > 0) this._settings.disconnect(this._settingsConnectionId);
+		this._settings = null;
+		if (this._ovhidingConnectionId > 0) Main.overview.disconnect(this._ovHidingConnectionId);
+		// disconnects from all loaded icons
+		for (let i = 0; i < this._tooltips.length; i++) {
+			this._tooltips[i].actor.disconnect(this._tooltips[i].con_d);
+			this._tooltips[i].actor.disconnect(this._tooltips[i].con_h);
+			this._tooltips[i].actor.disconnect(this._tooltips[i].con_focus_in);
+			this._tooltips[i].actor.disconnect(this._tooltips[i].con_focus_out);
 		}
-		_connect(actor);
+		this._tooltips=null;
 	}
 
-}
+	_applySettings() {
+		LABELSHOWTIME = this._settings.get_int("labelshowtime")/100 ;
+		LABELHIDETIME = this._settings.get_int("labelhidetime")/100 ;
+		HOVERDELAY = this._settings.get_int("hoverdelay") ;
+		TITLE = this._settings.get_boolean("title") ;
+		APPDESCRIPTION = this._settings.get_boolean("appdescription") ;
+		GROUPAPPCOUNT = this._settings.get_boolean("groupappcount") ;
+		BORDERS = this._settings.get_boolean("borders");
+		KEYBOARD = this._settings.get_boolean("keyboard");
+	}
 
+	// Tooltip manipulation
 
-function _connect(actor) {
-
-	_tooltips.push({
-		'actor': actor,
-		'con_focus_in': actor.connect('key-focus-in', _onHover),
-		'con_focus_out': actor.connect('key-focus-out', _onHover),
-		'con_h': actor.connect('notify::hover', _onHover),
-		'con_d': actor.connect('destroy', _onDestroy)
-	});
-
-}
-
-
-function _onDestroy(actor){
-
-	// This AppIcon is being destroy, let's forget about it
-	// so we don't try to disconnect from it later
-	_tooltips = _tooltips.filter( (item) => (item.actor !== actor) );
-
-}
-
-
-function _onHover(actor){
-
-	// checks if cursor is over the icon
-	if (actor.get_hover() || ( KEYBOARD && actor.has_key_focus() )) {
-	
-		// it is : let's setup a toolip display
-		// unless it's already set
-		if (_labelTimeoutId == 0) {
-
-			// if the tooltip is already displayed (on another icon)
-			// we update it, else we delay it
-			if (_labelShowing) {
-				_showTooltip(actor);
-			} else {
-				_labelTimeoutId = Mainloop.timeout_add(HOVERDELAY, function() {
-					_showTooltip(actor);
-					_labelTimeoutId = 0;
-					return false;
-				} );
+	_showTooltip(actor) {
+		// check if actor is still relevant, it may have been destroyed
+		// between hover event and tooltip display (there's a small delay)
+		// Skipping this test may lead to segfault
+		if (! this._tooltips.find( (item) => (item.actor === actor) )) {
+			return;
+		}
+		let icontext = '';
+		let titletext = '';
+		let detailtext = '';
+		let should_display = false;
+		if (actor._delegate.app){
+			//applications overview
+			icontext = actor._delegate.app.get_name();
+			if (APPDESCRIPTION) {
+				let appDescription = actor._delegate.app.get_description();
+				if (appDescription){
+					detailtext = appDescription;
+					should_display = true;
+				}
 			}
-
-		}
-
-	} else {
-	
-		// cursor is no more on an icon
-		_onLeave();
-
-	}
-
-}
-
-
-function _onLeave() {
-
-	// unset label display timer if needed
-	if (_labelTimeoutId > 0){
-		Mainloop.source_remove(_labelTimeoutId);
-		_labelTimeoutId = 0;
-	}
-
-	if (_labelShowing) {
-		_hideTooltip();
-		_labelShowing = false;
-	}
-}
-
-
-function _showTooltip(actor) {
-
-	let icontext = '';
-	let titletext = '';
-	let detailtext = '';
-	let should_display = false;
-
-	// check if actor is still relevant, it may have been destroyed
-	// between hover event and tooltip display (there's a small delay)
-	// Skipping this test may lead to segfault
-	if (! _tooltips.find( (item) => (item.actor === actor) )) {
-		return;
-	}
-
-	if (actor._delegate.app){
-		//applications overview
-		icontext = actor._delegate.app.get_name();
-
-		if (APPDESCRIPTION) {
-			let appDescription = actor._delegate.app.get_description();
-			if (appDescription){
-				detailtext = appDescription;
+		} else if (actor._delegate.hasOwnProperty('_folder')){
+			// folder in the application overview
+			icontext = actor._delegate['name'];
+			if (GROUPAPPCOUNT) {
+				let appCount = actor._delegate.view.getAllItems().length;
+				detailtext = __( "Group of %d application", "Group of %d applications", appCount ).format(appCount);
 				should_display = true;
 			}
+		} else {
+			//app and settings searchs results
+			icontext = actor._delegate.metaInfo['name'];
 		}
-
-	} else if (actor._delegate.hasOwnProperty('_folder')){
-		// folder in the application overview
-
-		icontext = actor._delegate['name'];
-		if (GROUPAPPCOUNT) {
-			let appCount = actor._delegate.view.getAllItems().length;
-			detailtext = Gettext.ngettext( "Group of %d application", "Group of %d applications", appCount ).format(appCount);
+		// Decide wether to show title
+		if ( TITLE && icontext ) {
+			titletext = icontext;
 			should_display = true;
 		}
-
-	} else {
-		//app and settings searchs results
-		icontext = actor._delegate.metaInfo['name'];
-
-	}
-
-	// Decide wether to show title
-	if ( TITLE && icontext ) {
-		titletext = icontext;
-		should_display = true;
-	}
-
-	// If there's something to show ..
-	if ( ( titletext || detailtext ) && should_display ) {
-
-		// Create a new tooltip if needed
-		if (!_ttbox) {
-			let css_class = BORDERS ? 'app-tooltip-borders' : 'app-tooltip';
-			_ttbox = new St.Bin({ style_class: css_class });
-			_ttlayout = new St.BoxLayout({ vertical: true });
-			_ttlabel = new St.Label({ style_class: 'app-tooltip-title', text: titletext });
-			_ttdetail = new St.Label({ style_class: 'app-tooltip-detail', text: detailtext });
-			_ttlayout.add_child(_ttlabel);
-			_ttlayout.add_child(_ttdetail);
-			_ttbox.add_actor(_ttlayout);
-			
-			// we force text wrap on both labels
-			_ttlabel.clutter_text.line_wrap = true;
-			_ttlabel.clutter_text.line_wrap_mode = Pango.WrapMode.WORD;
-			_ttlabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-			_ttdetail.clutter_text.line_wrap = true;
-			_ttdetail.clutter_text.line_wrap_mode = Pango.WrapMode.WORD;
-			_ttdetail.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-
-			Main.uiGroup.add_actor(_ttbox);
-		} else {
-			_ttlabel.text = titletext;
-			_ttdetail.text = detailtext;
-		}
-
-		if (!titletext) { _ttlabel.hide() } else { _ttlabel.show() };
-		if (!detailtext) { _ttdetail.hide() } else { _ttdetail.show() };
-
-		let [stageX, stageY] = actor.get_transformed_position();
-		let [iconWidth, iconHeight] = actor.get_transformed_size();
-		let y = stageY + iconHeight + 5;
-		let x = stageX - Math.round((_ttbox.get_width() - iconWidth)/2);
-
-		// do not show label move if not in showing mode
-		if (_labelShowing) {
-
-			_ttbox.ease({
-				x: x,
-				y: y,
-				opacity: 255,
-				duration: SLIDETIME  * 100,
-				mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-			});
-
-		} else {
-
-			_ttbox.set_position(x, y);
-			_ttbox.ease({
-				x: x,
-				y: y,
-				opacity: 255,
-				duration: LABELSHOWTIME  * 100,
-				mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-			});
-			_labelShowing = true;
-
-		}
-
-	} else {
-
-		// No tooltip to show : act like we're leaving an icon
-		_onLeave();
-
-	}
-
-}
-
-
-function _hideTooltip() {
-
-	if (_ttbox){
-		_ttbox.ease({
-			opacity: 0,
-			duration: LABELHIDETIME  * 100,
-			mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-			onComplete: () => {
-				_ttlabel = null;
-				_ttdetail = null;
-				Main.uiGroup.remove_actor(_ttbox);
-				_ttbox = null;
+		// If there's something to show ..
+		if ( ( titletext || detailtext ) && should_display ) {
+			// Create a new tooltip if needed
+			if (!this._ttbox) {
+				let css_class = BORDERS ? 'app-tooltip-borders' : 'app-tooltip';
+				this._ttbox = new St.Bin({ style_class: css_class });
+				this._ttlayout = new St.BoxLayout({ vertical: true });
+				this._ttlabel = new St.Label({ style_class: 'app-tooltip-title', text: titletext });
+				this._ttdetail = new St.Label({ style_class: 'app-tooltip-detail', text: detailtext });
+				this._ttlayout.add_child(this._ttlabel);
+				this._ttlayout.add_child(this._ttdetail);
+				this._ttbox.add_actor(this._ttlayout);
+				// we force text wrap on both labels
+				this._ttlabel.clutter_text.line_wrap = true;
+				this._ttlabel.clutter_text.line_wrap_mode = Pango.WrapMode.WORD;
+				this._ttlabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
+				this._ttdetail.clutter_text.line_wrap = true;
+				this._ttdetail.clutter_text.line_wrap_mode = Pango.WrapMode.WORD;
+				this._ttdetail.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
+				// Add as a new child on screen
+				Main.uiGroup.add_actor(this._ttbox);
+			} else {
+				this._ttlabel.text = titletext;
+				this._ttdetail.text = detailtext;
 			}
+			// Show/Hide elements as needed
+			if (!titletext) { this._ttlabel.hide() } else { this._ttlabel.show() };
+			if (!detailtext) { this._ttdetail.hide() } else { this._ttdetail.show() };
+			// Compute tooltip location
+			let [stageX, stageY] = actor.get_transformed_position();
+			let [iconWidth, iconHeight] = actor.get_transformed_size();
+			let y = stageY + iconHeight + 5;
+			let x = stageX - Math.round((this._ttbox.get_width() - iconWidth)/2);
+			// do not show label move if not in showing mode
+			if (this._labelShowing) {
+				this._ttbox.ease({
+					x: x,
+					y: y,
+					opacity: 255,
+					duration: SLIDETIME  * 100,
+					mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+				});
+			} else {
+				this._ttbox.set_position(x, y);
+				this._ttbox.ease({
+					x: x,
+					y: y,
+					opacity: 255,
+					duration: LABELSHOWTIME  * 100,
+					mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+				});
+				this._labelShowing = true;
+			}
+		} else {
+			// No tooltip to show : act like we're leaving an icon
+			this._onLeave();
+		}
+	}
+
+	_hideTooltip() {
+		if (this._ttbox){
+			this._ttbox.ease({
+				opacity: 0,
+				duration: LABELHIDETIME  * 100,
+				mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+				onComplete: () => {
+					this._ttlabel = null;
+					this._ttdetail = null;
+					Main.uiGroup.remove_actor(this._ttbox);
+					this._ttbox = null;
+				}
+			});
+		}
+	}
+
+	// Events related to app icons
+
+	_connectAll(view) {
+		let appIcons = view._orderedItems;
+		for (let i in appIcons) {
+			let icon = appIcons[i];
+			let actor = icon;
+			if (actor._delegate.hasOwnProperty('_folder')) {
+				this._connectAll(icon.view)
+			}
+			this._connect(actor);
+		}
+	}
+	_connect(actor) {
+		this._tooltips.push({
+			'actor': actor,
+			'con_focus_in': actor.connect('key-focus-in', this._onHover.bind(this)),
+			'con_focus_out': actor.connect('key-focus-out', this._onHover.bind(this)),
+			'con_h': actor.connect('notify::hover', this._onHover.bind(this)),
+			'con_d': actor.connect('destroy', this._onDestroy.bind(this))
 		});
+	}
+	_onHover(actor) {
+		// checks if cursor is over the icon
+		if (actor.get_hover() || ( KEYBOARD && actor.has_key_focus() )) {
+			// it is : let's setup a toolip display
+			// unless it's already set
+			if (this._labelTimeoutId == 0) {
+				// if the tooltip is already displayed (on another icon)
+				// we update it, else we delay it
+				if (this._labelShowing) {
+					this._showTooltip(actor);
+				} else {
+					let that = this;
+					this._labelTimeoutId = GLib.timeout_add(
+						GLib.PRIORITY_DEFAULT,
+						HOVERDELAY,
+						function() {
+							that._showTooltip(actor);
+							that._labelTimeoutId = 0;
+							return false;
+						}
+					);
+				}
+			}
+		} else {
+			// cursor is no more on an icon
+			this._onLeave();
+		}
+	}
+	_onLeave() {
+		// unset label display timer if needed
+		if (this._labelTimeoutId > 0){
+			GLib.Source.remove(this._labelTimeoutId);
+			this._labelTimeoutId = 0;
+		}
+		if (this._labelShowing) {
+			this._hideTooltip();
+			this._labelShowing = false;
+		}
+	}
+	_onDestroy(actor) {
+		// This AppIcon is being destroy, let's forget about it
+		// so we don't try to disconnect from it later
+		this._tooltips = this._tooltips.filter( (item) => (item.actor !== actor) );
 	}
 
 }
--- prefs.js
+++ prefs.js
@@ -1,41 +1,42 @@
 /* Applications overview tooltip
  *
- * Preferences dialog for gnome-shell-extensions-prefs tool
+ * Preferences dialog
  */
- 
-const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
-const Gio = imports.gi.Gio;
-const Lang = imports.lang;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
 
-const Gettext = imports.gettext.domain('applications-overview-tooltip');
-const _ = Gettext.gettext;
+import Gio from "gi://Gio";
+import Gtk from 'gi://Gtk';
 
-let settings;
+import {ExtensionPreferences} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js"
 
-function init() {
-	settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.applications-overview-tooltip');
-	ExtensionUtils.initTranslations("applications-overview-tooltip");
+export default class ApplicationOverviewTooltipPreferences extends ExtensionPreferences {
+
+	fillPreferencesWindow(window) {
+		// Prepare labels and controls
+		let buildable = new Gtk.Builder();
+		buildable.add_from_file( this.dir.get_path() + '/prefs.xml' );
+
+		// Bind fields to settings
+		let settings = this.getSettings();
+		settings.bind('hoverdelay', buildable.get_object('field_hoverdelay'), 'value', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('labelshowtime', buildable.get_object('field_labelshowtime'), 'value', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('labelhidetime', buildable.get_object('field_labelhidetime'), 'value', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('appdescription', buildable.get_object('field_appdescription'), 'active', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('groupappcount', buildable.get_object('field_groupappcount'), 'active', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('borders', buildable.get_object('field_borders'), 'active', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('keyboard', buildable.get_object('field_keyboard'), 'active', Gio.SettingsBindFlags.DEFAULT);
+		settings.bind('title', buildable.get_object('field_title'), 'active', Gio.SettingsBindFlags.DEFAULT);
+
+		// Fill in about page from metadata
+		buildable.get_object('about_name').set_text(this.metadata.name.toString());
+		buildable.get_object('about_version').set_text(this.metadata.version.toString());
+		buildable.get_object('about_description').set_text(this.metadata.description.toString());
+		buildable.get_object('about_url').set_markup("<a href=\"" + this.metadata.url.toString() + "\">" + this.metadata.url.toString() + "</a>");
+
+		// Pref window layout
+		window.search_enabled = true;
+		window.add( buildable.get_object('page_basic') );
+		window.add( buildable.get_object('page_advanced') );
+		window.add( buildable.get_object('page_about') );
+	}
+
 }
-
-function buildPrefsWidget(){
-
-	// Prepare labels and controls
-	let buildable = new Gtk.Builder();
-	buildable.add_from_file( Me.dir.get_path() + '/prefs.xml' );
-	let box = buildable.get_object('prefs_widget');
-
-	// Bind fields to settings
-	settings.bind('hoverdelay', buildable.get_object('field_hoverdelay'), 'value', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('labelshowtime', buildable.get_object('field_labelshowtime'), 'value', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('labelhidetime', buildable.get_object('field_labelhidetime'), 'value', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('appdescription', buildable.get_object('field_appdescription'), 'active', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('groupappcount', buildable.get_object('field_groupappcount'), 'active', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('borders', buildable.get_object('field_borders'), 'active', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('keyboard', buildable.get_object('field_keyboard'), 'active', Gio.SettingsBindFlags.DEFAULT);
-	settings.bind('title', buildable.get_object('field_title'), 'active', Gio.SettingsBindFlags.DEFAULT);
-
-	return box;
-};
